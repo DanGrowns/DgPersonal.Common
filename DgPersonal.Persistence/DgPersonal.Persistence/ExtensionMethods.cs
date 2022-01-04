ba3df17eee7999aae3a62e13b28a11ba18cf876e
Dan <dgrowns@gmail.com>
2022-01-04 09:33:28 +0000

    Configure Persistence Objects

---
+++ DgPersonal.Persistence/DgPersonal.Persistence/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using DgPersonal.Persistence.Classes;
+using DgPersonal.Persistence.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DgPersonal.Persistence
+{
+    public static class ExtensionMethods
+    {
+        public static void ConfigurePersistenceObjects<TDbContext>(this IServiceCollection services)
+            where TDbContext : IDbContext
+        {
+            services.AddScoped(typeof(IDbContext), provider => provider.GetService<TDbContext>());
+            services.AddTransient<IDapperQueryHandler, DapperQueryHandler>();
+            services.AddTransient<IEntityFrameworkModelDeleter, EntityFrameworkModelDeleter>();
+            services.AddTransient(typeof(IEntityFrameworkModelEditor<,>), typeof(EntityFrameworkModelEditor<,>));
+            services.AddTransient(typeof(IEntityFrameworkDependentModelEditor<,>), typeof(EntityFrameworkDependentModelEditor<,>));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# DgPersonal.Common
-Generally useful code that I use across multiple projects.
+# DgPersonal.Persistence
+Generic objects for Dapper (reads) and EntityFramework (writes) used across multiple projects.
+
+Designed to be used behind an always valid domain boundary with the entity objects immutable outside of protected or internal scenarios.
